a1605b42c4fc84de97fd43da49a557364c8603db
MyCode07 <erem19992015@mail.ru>
2023-06-16 19:56:16 +0300

    add to-top btn

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ import './utils/mini-cart.js';
 import './utils/sliders.js';
 // import './utils/mouse.js';
 // import './utils/ticker.js';
-// import './utils/to-top.js';
+import './utils/to-top.js';
 // import './utils/popup.js';
 // import './utils/inputmask.js';
 // import './utils/forms.js';
